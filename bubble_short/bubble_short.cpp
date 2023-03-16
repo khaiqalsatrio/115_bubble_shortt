3cb13f4f55245d24d47a008fcd17ebaa6adb887d
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-03-16 19:18:40 +0700

    membuat prosedur array

--- bubble_short/bubble_short.cpp
+++ bubble_short/bubble_short.cpp
@@ -20,3 +20,20 @@ void input() {            // prosedur untuk input
 	cout << "====================" << endl;
 	cout << "Masukkan Elemen Array" << endl;
 	cout << "====================" << endl;
+	
+	for (int i = 0; i < n; i++) { // looping dengan 1 dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": "; // Output ke Layar
+		cin >> a[i];					// Input dari pengguna
+	}
+}
+void bubbleSortArray() {	//procedur untuk mengurutkan array dengan metode bubble}
+	for (int i = 1; i < n; i++) { //Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - i; j++) { //Looping dengan j dimulai dari 0 hingga n-i-1
+			if (a[j] > a[j + 1]) { // Jika nilai pada a{j} lebih besar dari a[j+l]
+				int temp = a[j];   //Simpan nilai a[j] ke variabel sementara tetap
+				a[j] = a[j + 1];   //assign nilai a[j+1] ke a[j]
+				a[j + 1] = temp;   //assogn nilai temp ke a[j+1]
+			}
+		}
+	}
+}
