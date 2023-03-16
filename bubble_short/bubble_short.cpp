36c272b4c3f519dd13e302864fec88f5b9945ea1
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-03-16 19:17:48 +0700

    membuat variable

--- bubble_short/bubble_short.cpp
+++ bubble_short/bubble_short.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main(){}
+
+int a[20];               //Deklarasi array a dengan ukuran 20 
+int n;                     //Deklarasi variasi n untuk menyimpan banyak elemen pada aray 
+
+
+void input() {            // prosedur untuk input 
+	while (true) {           // loping
+		cout << "masukan banyak elemen pada array : "; // Output ke larar 
+		cin >> n;         // input dari pengguna 
+		if (n <= 20)      // jika keluar dari atau sama dengan 20
+			break;        // keluar dari 20 
+		else {            // jika n lrbih dari 20
+			cout << "\nAray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "====================" << endl;
